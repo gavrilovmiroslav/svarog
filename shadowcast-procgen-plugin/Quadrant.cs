9662fff91dc83d2991e6e9ff986adf064318b21b
Isa1997 <stefan.i@hotmail.rs>
2024-10-21 21:40:27 +0200

    Add Shadowcast plugin

---
+++ shadowcast-procgen-plugin/Quadrant.cs
@@ -0,0 +1,48 @@
+﻿namespace shadowcast_procgen_plugin
+{
+    internal class Quadrant
+    {
+        internal enum Direction
+        {
+            North = 0,
+            East = 1,
+            South = 2,
+            West = 3
+        }
+
+        private Direction _direction;
+        private int _startX;
+        private int _startY;
+        public Quadrant(Direction direction, (int, int) coords)
+        {
+            _direction = direction;
+            _startX = coords.Item1;
+            _startY = coords.Item2;
+        }
+
+        public (int, int) Transform((int, int) tileCoords)
+        {
+            var row = tileCoords.Item1;
+            var column = tileCoords.Item2;
+
+            if (_direction == Direction.North)
+            {
+                return (_startX + column, _startY - row);
+            }
+            else if (_direction == Direction.South)
+            {
+                return (_startX + column, _startY + row);
+            }
+            else if (_direction == Direction.East)
+            {
+                return (_startX + row, _startY + column);
+            }
+            else if (_direction == Direction.West)
+            {
+                return (_startX - row, _startY + column);
+            }
+
+            return (0, 0);
+        }
+    }
+}
---
+++ shadowcast-procgen-plugin/RowData.cs
@@ -0,0 +1,31 @@
+﻿namespace shadowcast_procgen_plugin
+{
+    internal class RowData
+    {
+        public int Depth;
+        public float StartSlope;
+        public float EndSlope;
+
+        public RowData(int depth, float startSlope, float endSlope)
+        {
+            Depth = depth;
+            StartSlope = startSlope;
+            EndSlope = endSlope;
+        }
+
+        public RowData GetNextRowData()
+        {
+            return new RowData(Depth + 1, StartSlope, EndSlope);
+        }
+
+        public List<(int, int)> GetTiles()
+        {
+            var min_col = (int)Math.Floor(Depth * StartSlope + 0.5);
+            var max_col = (int)Math.Ceiling(Depth * EndSlope - 0.5);
+            if (min_col > max_col)
+                return new List<(int, int)>();
+
+            return new List<(int, int)>(Enumerable.Zip(Enumerable.Repeat(Depth, max_col - min_col + 1), Enumerable.Range(min_col, max_col - min_col + 1)));
+        }
+    }
+}
